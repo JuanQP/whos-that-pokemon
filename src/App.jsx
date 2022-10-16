412ad9d31ce9f0c11d194b33c3f3021aeeee93ae
JuanQP <quinterosp.juan@gmail.com>
2022-10-16 16:38:05 -0300

    Initial commit with MVP of the game

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { Button, Card, Container, Grid, Image, Text, Title } from '@mantine/core';
+import { useRef, useState } from 'react'
+import './App.css'
+import pokemons from './assets/pokemons.json';
+import { pickOptions, pickRandomPokemon } from './utils';
+
+const DELAY=2000;
+
+function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [randomPokemon, setRandomPokemon] = useState(null);
+  const [selected, setSelected] = useState(null);
+  const [options, setOptions] = useState([]);
+  const [successCount, setSuccessCount] = useState(0);
+  const timeout = useRef(null);
+
+  function handleNextPokemonClick() {
+    setIsPlaying(true);
+    setSelected(null);
+    const newRandomPokemon = pickRandomPokemon(pokemons, false);
+    const newOptions = pickOptions(pokemons, newRandomPokemon);
+    setRandomPokemon(newRandomPokemon);
+    setOptions(newOptions);
+  }
+
+  function handlePokemonOptionClick(selectedPokemon) {
+    if(selectedPokemon.id === randomPokemon.id) {
+      setSuccessCount(previous => previous + 1);
+    }
+    setSelected(selectedPokemon);
+
+    // Clear timeout and jump to next pokemon in DELAY seconds.
+    clearTimeout(timeout.current)
+    timeout.current = setTimeout(function() {
+      handleNextPokemonClick();
+    }, DELAY);
+  }
+
+  return (
+    <Container size="sm" mt="sm">
+      <Grid>
+        <Grid.Col xs={12}>
+          <Title>Who's that Pokémon?</Title>
+        </Grid.Col>
+        <Grid.Col xs={12}>
+          <Text>{successCount} wins</Text>
+        </Grid.Col>
+        <Grid.Col xs={12}>
+          <Button
+            variant="filled"
+            disabled={isPlaying}
+            onClick={handleNextPokemonClick}
+          >
+            {isPlaying ? "Already playing!" : "Play"}
+          </Button>
+        </Grid.Col>
+        {isPlaying && (
+        <>
+          <Grid.Col xs={0} md={3} />
+          <Grid.Col xs={12} md={6}>
+            <Card shadow="sm">
+              <Card.Section>
+                <Image
+                  className={selected ? 'image-selected-option' : 'image-waiting-option'}
+                  src={randomPokemon.imageSrc}
+                  alt="Pokemon"
+                  height={192}
+                  fit="contain"
+                />
+              </Card.Section>
+            </Card>
+          </Grid.Col>
+          <Grid.Col xs={0} md={3} />
+          {options.map(option => (
+            <Grid.Col key={option.id} xs={12} md={6}>
+              <Button
+                fullWidth
+                uppercase
+                size="md"
+                variant={selected?.id === option.id || (selected && option.id === randomPokemon.id) ? 'filled' : 'outline'}
+                color={selected && randomPokemon.id === option.id ? 'green' :
+                  selected?.id === option.id ? 'red' : undefined
+                }
+                onClick={() => handlePokemonOptionClick(option)}
+              >
+                {option.name}
+              </Button>
+            </Grid.Col>
+          ))}
+        </>
+        )}
+      </Grid>
+    </Container>
+  )
+}
+
+export default App
---
+++ src/utils.jsx
@@ -0,0 +1,29 @@
+export function randomNumber(from = 0, to = 0) {
+  return from + Math.floor(Math.random() * to);
+}
+
+export function pickRandomPokemon(pokemons = [], remove = false) {
+  if(remove) {
+    return pokemons.splice(randomNumber(0, pokemons.length), 1)[0];
+  }
+  return pokemons[randomNumber(0, pokemons.length)];
+}
+
+// https://sebhastian.com/shuffle-array-javascript/
+function shufflePokemons(pokemons) {
+  return pokemons.sort(() => Math.random() - 0.5);
+}
+
+export function pickOptions(pokemons = [], excludedPokemon = {}) {
+  const pokemonsExceptOne = pokemons.filter(pokemon => pokemon.id !== excludedPokemon.id);
+  const pokemon1 = pickRandomPokemon(pokemonsExceptOne, true);
+  const pokemon2 = pickRandomPokemon(pokemonsExceptOne, true);
+  const pokemon3 = pickRandomPokemon(pokemonsExceptOne, true);
+  const shuffledPokemons = shufflePokemons([
+    excludedPokemon,
+    pokemon1,
+    pokemon2,
+    pokemon3,
+  ]);
+  return shuffledPokemons;
+}
